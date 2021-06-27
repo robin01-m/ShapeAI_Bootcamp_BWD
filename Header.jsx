8e3ee408b456f558bcff0e9be199302bcabfabb1
robin01-m <73293636+robin01-m@users.noreply.github.com>
2021-06-27 15:53:22 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Bootacmp on Javascript and React</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React js Bootcamp</h1>
+      <p>A 7 Day Bootcamp to learn the Basics of Javascript 
+        and getting started with React</p>
+        </div>
+  );
+}
+
+export default Note;
